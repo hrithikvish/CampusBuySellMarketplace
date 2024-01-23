9f82bec3fbc6843010eff69d3f38bc65464ddf35
hrithikvish <hrithikvishwakarma05@gmail.com>
2024-01-23 22:46:18 +0530

    nothing changed

--- app/src/main/java/com/hrithikvish/cbsm/PhoneAuthActivity.java
+++ app/src/main/java/com/hrithikvish/cbsm/PhoneAuthActivity.java
@@ -3,6 +3,8 @@ package com.hrithikvish.cbsm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.hrithikvish.cbsm.databinding.ActivityPhoneAuthBinding;
 
@@ -16,6 +18,14 @@ public class PhoneAuthActivity extends AppCompatActivity {
         binding = ActivityPhoneAuthBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+
+        /*//create a list of items for the spinner.
+        String[] items = new String[]{"+91", "+92", "+93"};
+        //create an adapter to describe how the items are displayed, adapters are used in several places in android.
+        //There are multiple variations of this, but this is the basic variant.
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+        //set the spinners adapter to the previously created one.
+        binding.countryPhoneCodeSpinner.setAdapter(adapter);*/
 
 
     }
